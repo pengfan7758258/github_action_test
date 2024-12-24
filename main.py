93aed969d352e2710648b8afd3b34cb9ae9e694b
pengfan7758258 <929848421@qq.com>
2024-12-24 10:20:07 +0800

    black isort校验

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-from fastapi import FastAPI
-
 import os
+
+from fastapi import FastAPI
 
 app = FastAPI()
 
 
 @app.get("/")
-def read_root(): return {"Hello": "World"}
+def read_root():
+    return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None): return {"item_id": item_id, "q": q}
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
