06cc5c8f7af5713065e1d14affd4109b786ceaff
pengfan7758258 <929848421@qq.com>
2024-12-24 10:36:28 +0800

    commitizen没用

--- main.py
+++ main.py
@@ -14,3 +14,8 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/foods/{food_id}")
+def read_food(food_id: int, q: str = None):
+    return {"item_id": food_id, "q": q}
